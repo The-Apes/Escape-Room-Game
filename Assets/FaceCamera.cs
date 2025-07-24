6e930942621ff1ebe2596c15f0c5dbc23f61c399
Mzati Chikoko <mzatichikoko@gmail.com>
2025-07-25 01:29:45 +0200

    NPC Setup - Text

---
+++ Assets/FaceCamera.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private Camera _cam;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        _cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(_cam.transform);
+        transform.rotation = transform.rotation * Quaternion.Euler(0, 180, 0);
+        
+        float textSize = Vector3.Distance(transform.position, _cam.transform.position)/ 5;
+        
+        rectTransform.localScale = new Vector3(textSize, textSize, textSize);
+        //print distance between the camera and the object
+        print (Vector3.Distance(transform.position, _cam.transform.position));
+    }
+}
