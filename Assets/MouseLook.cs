9724a989174095ff157b43bd10c7e283e9f61a79
Phiwe Bhengu <bhengu080104@gmail.com>
2025-07-30 21:35:30 +0200

    Player Movement

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -6,11 +6,14 @@ public class MouseLook : MonoBehaviour
    public float mouseSensitivity = 100f;
 
     public Transform playerBody;
+
+    float xRotation = 0f;
    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked; // Locks the cursor to the center of the screen
+        Cursor.visible = false; // Makes the cursor invisible
     }
 
     // Update is called once per frame
@@ -19,6 +22,10 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
