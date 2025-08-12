4641709dc0c2bc069ed38b4107655ba84d7a1181
Mzati (Laptop) <mzatichikoko@gmail.com>
2025-08-12 11:47:47 +0200

    Update PickUp.cs

--- Assets/PickUp.cs
+++ Assets/PickUp.cs
@@ -12,7 +12,7 @@ public class PickUpScript : MonoBehaviour
 {
     public GameObject player;
     public Transform holdPos;
-    public float throwForce = 500f; //force at which the object is thrown at
+   // public float throwForce = 500f; //force at which the object is thrown at
     public float pickUpRange = 5f; //how far the player can pickup the object from
     private float rotationSensitivity = 1f; //how fast/slow the object is rotated in relation to mouse movement
     private GameObject heldObj; //object which we pick up
