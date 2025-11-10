cdff802edadc35c259ebd5bc539d4265ed3a3350
Mzati (PC) <mzatichikoko@gmail.com>
2025-11-10 20:19:15 +0200

    Add vent dialogue and update puzzle stage logic
    Introduced a new 'Open the Vent' dialogue asset and integrated it into the Keyboard interaction logic for puzzle progression. Updated dialogue scripts for puzzle stages 2 and 3 with new lines and adjusted choice references. Modified the TestNPCRoom scene and Keyboard.cs to support the new vent script and ensure correct stage transitions.

--- Assets/Scripts/Objects/Keyboard.cs
+++ Assets/Scripts/Objects/Keyboard.cs
@@ -8,6 +8,7 @@ namespace Objects
     public class Keyboard : MonoBehaviour, IInteractable
     {
         [SerializeField] private NpcScriptAsset lockedComputerScript;
+        [SerializeField] private NpcScriptAsset ventScript;
         
         [SerializeField] private GameObject greenScreen;
         [SerializeField] private GameObject redScreen;
@@ -22,11 +23,18 @@ namespace Objects
         {
             switch (PuzzleManager.instance.puzzleStage)
             {
-                case -1:
+                default:
                     return;
                 case 0:
                     redScreen.SetActive(true);
                     ScriptManager.instance.RunScript(lockedComputerScript);
+                    PuzzleManager.instance.SetPuzzleStage(1);
+                    break;
+                case 2:
+                    PuzzleManager.instance.SetPuzzleStage(3);
+                    redScreen.SetActive(false);
+                    greenScreen.SetActive(true);
+                    ScriptManager.instance.RunScript(ventScript);
                     break;
             }
         }
